fd65582b65f767f0c00574909d78fd4de20ab6fd
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-01-12 15:42:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList; // Import ArrayList
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputs = input.split(" ");
+            int[] arr = new int[inputs.length];
+
+            for (int i = 0; i < inputs.length; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
+            }
+
+            // Get the result as an ArrayList<Integer>
+            ArrayList<Integer> ans = new Solution().findTwoElement(arr);
+            System.out.println(ans.get(0) + " " + ans.get(1));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find two elements in array
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = arr.length;
+        int repeating = -1, missing = -1;
+
+        // Mark elements as visited
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(arr[i]) - 1;
+
+            // If the value at this index is already negative, it's the repeating number
+            if (arr[index] < 0) {
+                repeating = Math.abs(arr[i]);
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+
+        // Find the missing number
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        res.add(repeating);
+        res.add(missing);
+        return res;
+    }
+}
