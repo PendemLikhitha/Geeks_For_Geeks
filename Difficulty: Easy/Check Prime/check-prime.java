0d79d7b4e92b8e49a5092af13aa4ac45391dba48
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-02-24 17:58:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+//User function Template for Java
+
+public static boolean prime(int n){
+    if(n==1){
+        return false;
+    }
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0)
+        return false;
+    }
+    return true;
+    //Write your code here
+    //returns a boolean value 
+    
+}
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int n = scn.nextInt();
+            boolean ans = prime(n);
+            if(ans)
+            System.out.println("True");
+            else
+            System.out.println("False");
+        
+System.out.println("~");
+}
+        scn.close();
+    }
+}
+// } Driver Code Ends
