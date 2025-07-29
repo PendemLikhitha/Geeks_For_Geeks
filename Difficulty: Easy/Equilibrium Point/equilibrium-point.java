e775bf5dd692d302421a73b7d35ae74c77de4f57
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-07-29 19:36:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -1,58 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(
-                    token)); // Use Integer.parseInt to parse int values
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling equilibriumPoint() function
-            System.out.println(obj.findEquilibrium(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     // Function to find equilibrium point in the array.
     public static int findEquilibrium(int arr[]) {
-        int n = arr.length;
-        int total =0,preSum=0,j=0;
-        for(int i :arr){
-            total+=i;
+        // code here
+        int sumR = 0, sumL = 0;
+        for(int i:arr){
+            sumR += i;
         }
-        while(j<n){
-            preSum+=arr[j];
-            int remaining = total-preSum;
-            if((preSum-arr[j]) == remaining){
-                return j;
+        for(int i = 0; i < arr.length; i++){
+            sumR -= arr[i];
+            if(sumL == sumR){
+                return i;
             }
-            j++;
+            sumL += arr[i];
         }
         return -1;
     }
