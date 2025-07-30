4237ca7864081450bc4f042d4dc90675a7200419
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-07-30 21:13:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
@@ -1,79 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-public class Main {
-    public static void printList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-        System.out.println();
-        System.out.println("~");
-    }
-
-    // Insert node into the list in a sorted manner
-    public static Node insertSorted(Node head, int data) {
-        Node newNode = new Node(data);
-        if (head == null || head.data >= data) {
-            newNode.next = head;
-            return newNode;
-        }
-
-        Node current = head;
-        while (current.next != null && current.next.data < data) {
-            current = current.next;
-        }
-        newNode.next = current.next;
-        current.next = newNode;
-
-        return head;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
-        while (T-- > 0) {
-            Node head1 = null;
-            Node head2 = null;
-
-            // Reading first linked list input
-            String input1 = sc.nextLine();
-            String[] elems1 = input1.split(" ");
-            for (String elem : elems1) {
-                head1 = insertSorted(head1, Integer.parseInt(elem));
-            }
-
-            // Reading second linked list input
-            String input2 = sc.nextLine();
-            String[] elems2 = input2.split(" ");
-            for (String elem : elems2) {
-                head2 = insertSorted(head2, Integer.parseInt(elem));
-            }
-
-            // Merging sorted linked lists
-            Solution obj = new Solution();
-            Node mergedHead = obj.sortedMerge(head1, head2);
-            printList(mergedHead); // Print the merged sorted list
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 /*
   Node is defined as
     class Node
@@ -88,29 +12,24 @@ class Solution {
     Node sortedMerge(Node head1, Node head2) {
         // code here
         Node dummy = new Node(-1);
-        Node current = dummy;
-
-        // Traverse both lists and merge them
-        while (head1 != null && head2 != null) {
-            if (head1.data <= head2.data) {
-                current.next = head1;
+        Node cur = dummy;
+        while(head1 != null && head2!= null){
+            if(head1.data <= head2.data){
+                cur.next = head1;
                 head1 = head1.next;
-            } else {
-                current.next = head2;
-                head2 = head2.next;
             }
-            current = current.next;
+            else{
+                cur.next = head2;
+                head2= head2.next;
+            }
+            cur = cur.next;
         }
-
-        // Attach remaining nodes, if any
-        if (head1 != null) {
-            current.next = head1;
+        if(head1 != null){
+            cur.next = head1;
         }
-        if (head2 != null) {
-            current.next = head2;
+        if(head2 != null){
+            cur.next = head2;
         }
-
-        // Return the head of the merged list
         return dummy.next;
     }
 }
